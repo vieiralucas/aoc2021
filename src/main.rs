82fcd0aa3035aafd178fdc2217a86f3f3db8e9f0
Lucas Vieira <vieiralucas4@gmail.com>
2021-12-19 12:27:44 -0300

    Day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use std::fs;
+
+fn day1_part1() {
+    let input: Vec<u32> = fs::read_to_string("inputs/day1/input.txt")
+        .expect("Failed to read input")
+        .lines()
+        .map(|l| l.parse().expect("Failed to convert line to u32"))
+        .collect();
+
+    let mut result = 0;
+    for (i, curr) in input.iter().enumerate() {
+        if i < 1 {
+            continue;
+        }
+
+        if let Some(prev) = input.get(i - 1) {
+            if prev < curr {
+                result += 1;
+            }
+        }
+    }
+
+    println!("Part 1: {}", result);
+}
+
+fn day1_part2() {
+    let input: Vec<u32> = fs::read_to_string("inputs/day1/input.txt")
+        .expect("Failed to read input")
+        .lines()
+        .map(|l| l.parse().expect("Failed to convert line to u32"))
+        .collect();
+
+    let mut result = 0;
+    for i in 0..input.len() {
+        if i < 3 {
+            continue;
+        }
+
+        let a: u32 = input[i - 3..i].iter().sum();
+        let b: u32 = input[i - 2..i + 1].iter().sum();
+
+        if a < b {
+            result += 1;
+        }
+    }
+
+    println!("Part 2: {}", result);
+}
+
 fn main() {
-    println!("Hello, world!");
+    day1_part1();
+    day1_part2();
 }
