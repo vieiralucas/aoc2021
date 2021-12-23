702b27f5c2829f0cfe707fdfb39a74364b199d1e
Lucas Vieira <vieiralucas4@gmail.com>
2021-12-23 13:41:11 -0300

    Day 3 part 2

--- src/main.rs
+++ src/main.rs
@@ -142,7 +142,92 @@ fn day3_part1() {
     println!("Day 3, part 1: {}", gamma * epsilon);
 }
 
-fn day3_part2() {}
+fn day3_part2() {
+    let mut bits: Vec<Vec<char>> = vec![];
+
+    for line in fs::read_to_string("inputs/day3/input.txt")
+        .expect("Failed to read input")
+        .lines()
+    {
+        bits.push(line.chars().clone().collect());
+    }
+
+    let mut oxygen_bits = bits.clone();
+    let mut i = 0;
+    while oxygen_bits.len() > 1 {
+        let mut ones = 0;
+        let mut zeroes = 0;
+
+        for bits in &oxygen_bits {
+            if bits[i] == '1' {
+                ones += 1;
+            } else {
+                zeroes += 1
+            }
+        }
+
+        let mut next_bits: Vec<Vec<char>> = Vec::new();
+
+        for bits in &oxygen_bits {
+            if zeroes > ones {
+                if bits[i] == '0' {
+                    next_bits.push(bits.clone());
+                }
+            } else {
+                if bits[i] == '1' {
+                    next_bits.push(bits.clone());
+                }
+            }
+        }
+
+        oxygen_bits = next_bits;
+
+        i += 1;
+    }
+
+    i = 0;
+    let mut co2_bits = bits.clone();
+    while co2_bits.len() > 1 {
+        let mut ones = 0;
+        let mut zeroes = 0;
+
+        for bits in &co2_bits {
+            if bits[i] == '1' {
+                ones += 1;
+            } else {
+                zeroes += 1
+            }
+        }
+
+        let mut next_bits: Vec<Vec<char>> = Vec::new();
+
+        for bits in &co2_bits {
+            if zeroes <= ones {
+                if bits[i] == '0' {
+                    next_bits.push(bits.clone());
+                }
+            } else {
+                if bits[i] == '1' {
+                    next_bits.push(bits.clone());
+                }
+            }
+        }
+
+        co2_bits = next_bits;
+
+        i += 1;
+    }
+
+    assert!(oxygen_bits.len() == 1);
+    assert!(co2_bits.len() == 1);
+
+    let oxygen = u32::from_str_radix(&String::from_iter(&oxygen_bits[0]), 2)
+        .expect("Failed to parse oxygen_bits");
+    let co2 = u32::from_str_radix(&String::from_iter(&co2_bits[0]), 2)
+        .expect("Failed to parse bits co2_bits");
+
+    println!("Day 3, part 2: {}", oxygen * co2);
+}
 
 fn main() {
     day1_part1();
