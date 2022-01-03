c36c8085b1da0b9d3c0fd187f0f979e8dc9b36a5
Lucas Vieira <vieiralucas4@gmail.com>
2022-01-03 19:33:44 -0300

    Day 6 part 2

--- src/day06.rs
+++ src/day06.rs
@@ -1,35 +1,28 @@
-pub fn part1() {
-    let mut fishes: Vec<usize> = include_str!("../inputs/day6/input.txt")
+fn solve(days: usize) -> usize {
+    let mut fishes: [usize; 9] = include_str!("../inputs/day6/input.txt")
         .trim()
         .split(',')
         .map(|str| str.parse().expect("a usize"))
-        .collect();
+        .fold([0; 9], |mut fishes, fish: usize| {
+            fishes[fish] += 1;
+            fishes
+        });
 
-    let mut next_fishes: Vec<usize> = fishes.clone();
-
-    for _ in 0..80 {
-        for (i, fish) in fishes.iter().enumerate() {
-            if *fish == 0 {
-                next_fishes[i] = 6;
-                next_fishes.push(8);
-            } else {
-                next_fishes[i] = fish - 1;
-            }
-        }
-
-        fishes = next_fishes.clone();
+    for _ in 0..days {
+        let ready = fishes[0];
+        fishes[0] = 0;
+        fishes.rotate_left(1);
+        fishes[8] = ready;
+        fishes[6] += ready;
     }
 
-    println!("Day 6, part 1: {}", fishes.len());
+    fishes.iter().sum()
+}
+
+pub fn part1() {
+    println!("Day 6, part 1: {}", solve(80));
 }
 
 pub fn part2() {
-    let _fishes: Vec<usize> = include_str!("../inputs/day6/sample.txt")
-        .trim()
-        .split(',')
-        .map(|str| str.parse().expect("a usize"))
-        .collect();
-
-    println!("Day 6, part 2: {}", 0);
+    println!("Day 6, part 2: {}", solve(256));
 }
-
